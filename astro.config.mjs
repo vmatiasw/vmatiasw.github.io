7878c4a25d93e68ad5def2aaf26df9831b09e7e2
Matias Viola <matiasvioladi@gmail.com>
2024-12-17 14:47:22 -0300

    init

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import astroIcon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://vmatiasw.github.io',
+  base: '/astro-icon/',
+  integrations: [
+    tailwind(),
+    astroIcon({
+      include: {
+        mdi: ["*"],
+        'ri': ['*'],
+        'simple-icons': ['*'],
+      },
+    })
+  ],
+});
