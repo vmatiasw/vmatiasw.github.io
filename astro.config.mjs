f4d6c2b7a436571cf58a4180e3d17179cc757721
Matias Viola <matiasvioladi@gmail.com>
2024-12-17 15:26:20 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import astroIcon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vmatiasw.github.io',
-  base: '/astro-icon/',
   integrations: [
     tailwind(),
     astroIcon({
