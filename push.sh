873dda452c0317454c8bb0189c662fe3208bad02
Matias Viola <matiasvioladi@gmail.com>
2024-12-21 21:16:03 -0300

    ejecutable para push

---
+++ push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+  echo "Error: se requiere un argumento de tipo string"
+  exit 1
+fi
+if [[ (! "$1" =~ ^\'[^\']*\'$) && (! "$1" =~ ^\"[^\"]*\"$) ]]; then
+  echo "Error: El argumento $1 debe empezar y terminar con comillas simples (') o dobles (\")."
+  exit 1
+fi
+
+npm run build
+if [ $? -ne 0 ]; then
+  # Si falla, salir
+  exit 1
+fi
+rm -rf ./dist
+git add .
+git commit -m "${1:1:-1}"
+git push
