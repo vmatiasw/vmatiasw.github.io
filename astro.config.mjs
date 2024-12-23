708bc24ac8ab0a6eca8ed3ed1e98b945dae21e70
Matias Viola <matiasvioladi@gmail.com>
2024-12-23 15:47:30 -0300

    refact(formateo)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import astroIcon from "astro-icon";
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
