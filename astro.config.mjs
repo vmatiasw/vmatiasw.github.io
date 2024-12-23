97197aace37158369faa4eacb432eefb3d786c95
Matias Viola <matiasvioladi@gmail.com>
2024-12-23 15:45:21 -0300

    refact(react):se integro react y se inicio para los filtros

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import astroIcon from "astro-icon";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vmatiasw.github.io",
   integrations: [
+    react(),
     tailwind(),
     astroIcon({
       include: {
--- src/CVRepository.ts
+++ src/CVRepository.ts
@@ -12,12 +12,68 @@ const dataSource: CV = JSON.parse(fs.readFileSync("./cv.json", "utf-8"));
 
 class CVRepository implements CV {
   public basics: Basics = dataSource.basics;
-  public work: Work[] = dataSource.work;
-  public education: Education[] = dataSource.education;
-  public projects: Projects[] = dataSource.projects;
+  private _work: Work[] = dataSource.work;
+  private _education: Education[] = dataSource.education;
+  private _projects: Projects[] = dataSource.projects;
   public skills: Skills = this.extractSkills();
+  private selectedSkills: Skills = structuredClone(this.skills);
+
+  public get work(): Work[] {
+    return this._work;
+  }
+
+  public get education(): Education[] {
+    return this._education;
+  }
+
+  public get projects(): Projects[] {
+    return this._projects;
+  }
+
+  public removeAllSkills(): void {
+    this.selectedSkills = {
+      languages: [],
+      tools: [],
+      soft: [],
+    };
+  }
+
+  public addAllSkills(): void {
+    this.selectedSkills = structuredClone(this.skills);
+  }
+
+  public addSkills(skills: Skills): void {
+    this.mergeSkills(this.selectedSkills.languages || [], skills.languages || []);
+    this.mergeSkills(this.selectedSkills.tools || [], skills.tools || []);
+    this.mergeSkills(this.selectedSkills.soft || [], skills.soft || []);
+  }
+
+  public removeSkills(skills: Skills): void {
+    this.removeSkillsFromList(this.selectedSkills.languages || [], skills.languages || []);
+    this.removeSkillsFromList(this.selectedSkills.tools || [], skills.tools || []);
+    this.removeSkillsFromList(this.selectedSkills.soft || [], skills.soft || []);
+  }
 
   // ------------------------- Private -------------------------
+  private removeSkillsFromList(target: NameAndIcon[], source: NameAndIcon[]): void {
+    source.forEach((skill) => {
+      const existingSkill = target.find((s) => s.name === skill.name);
+      if (existingSkill) {
+        this.removeSkill(existingSkill, skill);
+      }
+    });
+  }
+
+  private removeSkill(target: any, source: any): void {
+    Object.keys(source).forEach((key) => {
+      if (Array.isArray(source[key])) {
+        target[key] = target[key].filter((item: any) => !source[key].includes(item));
+      } else {
+        delete target[key];
+      }
+    });
+  }
+
   private extractSkills(): Skills {
     const skills: Skills = {
       languages: [],
@@ -25,25 +81,25 @@ class CVRepository implements CV {
       soft: [],
     };
 
-    this.extendSkills(skills, this.work);
-    this.extendSkills(skills, this.education);
-    this.extendSkills(skills, this.projects);
+    this.collectSkillsFromItems(skills, this._work);
+    this.collectSkillsFromItems(skills, this._education);
+    this.collectSkillsFromItems(skills, this._projects);
 
     return skills;
   }
 
-  private extendSkills(
+  private collectSkillsFromItems(
     skills: Skills,
     items: Array<Work | Education | Projects>,
   ): void {
     items.forEach((item) => {
-      this.addSkills(skills.languages || [], item.skills?.languages);
-      this.addSkills(skills.tools || [], item.skills?.tools);
-      this.addSkills(skills.soft || [], item.skills?.soft);
+      this.mergeSkills(skills.languages || [], item.skills?.languages);
+      this.mergeSkills(skills.tools || [], item.skills?.tools);
+      this.mergeSkills(skills.soft || [], item.skills?.soft);
     });
   }
 
-  private addSkills(target: NameAndIcon[], source: NameAndIcon[] = []): void {
+  private mergeSkills(target: NameAndIcon[], source: NameAndIcon[] = []): void {
     source.forEach((skill) => {
       const existingSkill = target.find((s) => s.name === skill.name);
       if (existingSkill) {
