b30235a852ccf5878f1542e9c32f12ebb6e878f8
Matias Viola <matiasvioladi@gmail.com>
2024-12-29 23:17:52 -0300

    refact(CVRepository)

--- src/CVRepository.ts
+++ src/CVRepository.ts
@@ -9,16 +9,11 @@ import type {
   Language,
 } from "./cv-types";
 import fs from "fs";
+
 const dataSource: CV = JSON.parse(fs.readFileSync("./cv.json", "utf-8"));
 
-interface CVSkill extends NameAndIcon {
-  name: string;
-  icon: string;
-  classId: string;
-}
-interface CVLanguage extends CVSkill {
-  technologies?: string[];
-}
+type CVSkill = NameAndIcon;
+type CVLanguage = Language;
 type SkillMap = { [name: string]: CVSkill };
 type LanguageMap = { [name: string]: CVLanguage };
 
@@ -28,24 +23,32 @@ interface CVSkills {
 }
 
 class CVRepository implements CV {
-  public basics: Basics = dataSource.basics;
-  public work: Work[] = dataSource.work;
-  public education: Education[] = dataSource.education;
-  public projects: Projects[] = dataSource.projects;
-  private skills: CVSkills = this.collectAllSkills();
+  public readonly basics: Basics;
+  public readonly work: Work[];
+  public readonly education: Education[];
+  public readonly projects: Projects[];
+  private skills: CVSkills;
 
-  public get languages(): CVLanguage[] {
+  constructor(data: CV) {
+    this.basics = data.basics;
+    this.work = data.work;
+    this.education = data.education;
+    this.projects = data.projects;
+    this.skills = this.collectAllSkills();
+  }
+
+  public get skillLanguages(): CVLanguage[] {
     return Object.values(this.skills.languages);
   }
 
-  public languageTecnologies(language: CVLanguage): string[] | null {
+  public getLanguageTecnologies(language: CVLanguage): string[] | null {
     if (!language.technologies) {
       return null;
     }
-    return language.technologies;
+    return [...language.technologies];
   }
 
-  public get tools(): CVSkill[] {
+  public get skillTools(): CVSkill[] {
     return Object.values(this.skills.tools);
   }
 
@@ -54,17 +57,13 @@ class CVRepository implements CV {
   }
 
   public getFilterClassNames(skills: Skills): string {
-    const mapSkillClassIds = (
-      skillMap: SkillMap,
-      skillList: NameAndIcon[] = [],
-    ) => skillList.map((skill) => skillMap[skill.name].classId) || [];
+    const toolsClassId =
+      skills.tools?.map((skill) => this.createFilterID("tools", skill.name)) ||
+      [];
 
-    const mapLanguagesClassIds = (
-      skillMap: LanguageMap,
-      skillList: Language[] = [],
-    ) =>
-      skillList.map((skill) => {
-        const langClassId = skillMap[skill.name].classId;
+    const languagesClassId =
+      skills.languages?.map((skill) => {
+        const langClassId = this.createFilterID("languages", skill.name);
         if (skill.technologies) {
           return [
             langClassId,
@@ -74,14 +73,7 @@ class CVRepository implements CV {
           ].join(" ");
         }
         return langClassId;
-      });
-
-    const toolsClassId = mapSkillClassIds(this.skills.tools, skills.tools);
-
-    const languagesClassId = mapLanguagesClassIds(
-      this.skills.languages,
-      skills.languages,
-    );
+      }) || [];
 
     return [...languagesClassId, ...toolsClassId, "skill-filter-item"].join(
       " ",
@@ -92,44 +84,31 @@ class CVRepository implements CV {
   private collectAllSkills(): CVSkills {
     const allSkills: CVSkills = { languages: {}, tools: {} };
 
-    [this.work, this.education, this.projects].forEach((items) =>
-      this.aggregateSkills(allSkills, items),
-    );
+    const source: Skills[] = [
+      ...this.work.map((item) => ({ ...item.skills })),
+      ...this.education.map((item) => ({ ...item.skills })),
+      ...this.projects.map((item) => ({ ...item.skills })),
+    ];
+
+    source.forEach((skill) => {
+      this.addSkillsToCategory(allSkills.languages, skill.languages);
+      this.addSkillsToCategory(allSkills.tools, skill.tools);
+    });
 
     return allSkills;
-  }
-
-  private aggregateSkills(
-    skills: CVSkills,
-    items: Array<Work | Education | Projects>,
-  ): void {
-    items.forEach((item) => {
-      this.addSkillsToCategory(
-        skills.languages || {},
-        item.skills?.languages,
-        "languages",
-      );
-      this.addSkillsToCategory(skills.tools || {}, item.skills?.tools, "tools");
-    });
   }
 
   private addSkillsToCategory(
     target: SkillMap,
     source: NameAndIcon[] = [],
-    category: string,
   ): void {
     source.forEach((skill) => {
       if (target[skill.name]) {
-        const existingSkill = target[skill.name];
-        if (existingSkill) {
-          this.mergeSkillProperties(existingSkill, skill);
-        }
+        const existingSkill = { ...target[skill.name] };
+        this.mergeSkillProperties(existingSkill, skill);
+        target[skill.name] = existingSkill;
       } else {
-        const cvSkill: CVSkill = {
-          ...skill,
-          classId: this.createFilterID(category, skill.name),
-        };
-        target[skill.name] = cvSkill;
+        target[skill.name] = { ...skill };
       }
     });
   }
@@ -144,8 +123,12 @@ class CVRepository implements CV {
     });
   }
 }
-
 // This instance of CVRepository is created once during the build process.
 // The data is pre-calculated and included in the static files, improving performance.
-const cvRepository = new CVRepository();
+const cvRepository = new CVRepository(dataSource);
 export { cvRepository as CV };
+
+// console.assert(
+//   JSON.stringify(dataSource) === fs.readFileSync("./cv.json", "utf-8"),
+//   "El objeto dataSource fue modificado.",
+// );
