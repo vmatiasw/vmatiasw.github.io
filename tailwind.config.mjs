4d56506cbf819fc64609e6821468182e223b733a
Matias Viola <matiasvioladi@gmail.com>
2025-01-12 18:45:58 -0300

    feat(fonts taiwind css)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 
-function withOpacity(variableName) {
-  return ({ opacityValue }) => {
-    if (opacityValue !== undefined) {
-      return `rgba(var(${variableName}), ${opacityValue})`;
-    }
-    return `rgb(var(${variableName}))`;
-  };
-}
-
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -36,10 +27,39 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    function ({ addVariant }) {
-      addVariant("not-print", "@media not print");
-      addVariant("below-md", "@media (max-width: 768px)");
-    },
-  ],
+  plugins: [addCustomVariants, addCustomBaseStyles],
 };
+
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
+function addCustomVariants({ addVariant }) {
+  addVariant("not-print", "@media not print");
+  addVariant("below-md", "@media (max-width: 768px)");
+}
+
+function addCustomBaseStyles({ addBase }) {
+  addBase({
+    h1: { fontSize: "2.25rem", lineHeight: "2.5rem" }, // text-4xl
+    h2: { fontSize: "1.875rem", lineHeight: "2.25rem" }, // text-3xl
+    h3: { fontSize: "1.25rem", lineHeight: "2rem" }, // text-xl
+    h4: { fontSize: "1.125rem", lineHeight: "1.75rem" }, // text-lg
+    h5: { fontSize: "1rem", lineHeight: "1.5rem" }, // text-base
+    h6: { fontSize: "1rem", lineHeight: "1.25rem" }, // text-base
+    p: { fontSize: "1rem", lineHeight: "1.25rem" }, // text-base
+    em: { fontSize: "1rem", lineHeight: "1.25rem" }, // text-base
+    "@media print": {
+      h1: { fontSize: "1.875rem", lineHeight: "2rem" }, // text-3xl
+      h2: { fontSize: "1.5rem", lineHeight: "1.75rem" }, // text-2xl
+      h3: { fontSize: "1rem", lineHeight: "1.25rem" }, // text-base
+      h4: { fontSize: "1rem", lineHeight: "1.25rem" }, // text-base
+      h5: { fontSize: "1rem", lineHeight: "1.25rem" }, // text-base
+    },
+  });
+}
