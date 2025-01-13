cb2decf14e4a3638d94d2946c842e53d7d187906
Matias Viola <matiasvioladi@gmail.com>
2025-01-13 16:11:58 -0300

    se saco h6

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -49,18 +49,17 @@ function addCustomBaseStyles({ addBase }) {
   addBase({
     h1: { fontSize: "2.25rem", lineHeight: "2.5rem" }, // text-4xl
     h2: { fontSize: "1.875rem", lineHeight: "2.25rem" }, // text-3xl
-    h3: { fontSize: "1.25rem", lineHeight: "2rem" }, // text-xl
-    h4: { fontSize: "1.125rem", lineHeight: "1.75rem" }, // text-lg
-    h5: { fontSize: "1rem", lineHeight: "1.5rem" }, // text-base
-    h6: { fontSize: "1rem", lineHeight: "1.25rem" }, // text-base
+    h3: { fontSize: "1.25rem", lineHeight: "1.75rem" }, // text-xl
+    h4: { fontSize: "1.125rem", lineHeight: "1.5rem" }, // text-lg
+    h5: { fontSize: "1rem", lineHeight: "1.25rem" }, // text-base
     body: { fontSize: "1rem", lineHeight: "1.25rem" }, // text-base
     "@media print": {
       h1: { fontSize: "1.875rem", lineHeight: "2rem" }, // text-3xl
       h2: { fontSize: "1.5rem", lineHeight: "1.75rem" }, // text-2xl
-      h3: { fontSize: "1rem", lineHeight: "1.25rem" }, // text-base
+      h3: { fontSize: "1.125rem", lineHeight: "1.25rem" }, // text-lg
       h4: { fontSize: "1rem", lineHeight: "1.25rem" }, // text-base
-      h5: { fontSize: "1rem", lineHeight: "1.25rem" }, // text-base
-      body: { fontSize: "0.875rem", lineHeight: "1.25rem" }, // text-sm
+      h5: { fontSize: "0.875rem", lineHeight: "1.25rem" }, // text-sm
+      body: { fontSize: "0.875rem", lineHeight: "1rem" }, // text-sm
     },
   });
 }
