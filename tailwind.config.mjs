4686da3e8ef30a12b2aabf57415b1ab234d06211
Matias Viola <matiasvioladi@gmail.com>
2025-02-07 20:01:50 -0300

    refact(tailwind base.css): text height and directories

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -28,7 +28,7 @@ export default {
       },
     },
   },
-  plugins: [addCustomVariants, addCustomBaseStyles],
+  plugins: [addCustomVariants],
 };
 
 function withOpacity(variableName) {
@@ -46,16 +46,4 @@ function addCustomVariants({ addVariant }) {
   addVariant("not-print", "@media not print &");
   addVariant("below-md", "@media (max-width: 768px)");
   addVariant("below-sm", "@media (max-width: 640px)");
-}
-
-function addCustomBaseStyles({ addBase }) {
-  addBase({
-    h1: { fontSize: "2rem", lineHeight: "2.5rem" }, // text-4xl
-    h2: { fontSize: "1.875rem", lineHeight: "2.25rem" }, // text-3xl
-    h3: { fontSize: "1.5rem", lineHeight: "2rem" }, // text-2xl
-    h4: { fontSize: "1.25rem", lineHeight: "1.75rem" }, // text-xl
-    h5: { fontSize: "1.125rem", lineHeight: "1.5rem" }, // text-lg
-    h6: { fontSize: "1rem", lineHeight: "1.4rem" }, // text-base
-    body: { fontSize: "1rem", lineHeight: "1.4rem" }, // text-base
-  });
 }
